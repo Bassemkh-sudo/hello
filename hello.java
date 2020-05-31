1eacb7536d9ba70a8b9e2b95197f10ca86708e4e
Bassemkh-sudo <66188530+Bassemkh-sudo@users.noreply.github.com>
2020-05-31 02:42:55 +0100

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,14 @@
+
+public class hello {
+
+public static void main(String[] arg){
+
+	System.out.println("Hello openclassrooms");
+	int nmb1 = 5 ;
+	int nmb2 = 4 ;
+	System.out.println(nmb2+nmb1);
+	sskkkk
+
+}
+
+}
